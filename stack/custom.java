dd6e497e42b9b8311c13528a1ce8731a79445078
rajveer0104 <163584756+rajveer0104@users.noreply.github.com>
2026-08-17 23:59:14 +0530

    Dynamic Stack added

--- stack/custom.java
+++ stack/custom.java
@@ -18,14 +18,13 @@ class custom_stack {
     public boolean isEmpty(){
         return top==-1;
     }
-    public boolean push(int x){
+    public void push(int x){
         if(isFull()){
             System.out.println("Stack is full");
-            return false;
+            return;
         }
         top++;
         data[top]=x;
-        return true;
 
     }
     public int pop() throws Exception{
@@ -44,15 +43,45 @@ class custom_stack {
     }
     
 }
+class dynamic_stack extends custom_stack{
+    public dynamic_stack(int size){
+        super(size);
+    }
+    public void push(int x){
+        if(this.isFull()){
+            int temp[]=new int[this.data.length*2];
+            for(int i =0;i<data.length;i++){
+                temp[i]=data[i];
+            }
+            data=temp;
+        }
+        super.push(x);
+
+    }
+}
 public class custom {
     public static void main(String[] args) throws Exception {
-        custom_stack stack=new custom_stack();
+        System.out.println("Normal Stack:::");
+        custom_stack stack=new custom_stack(4);
         stack.push(5);
         stack.push(6);
         stack.push(7);
         stack.push(8);
+        stack.push(9);
         System.out.println(stack.pop());
         System.out.println(stack.pop());
         System.out.println(stack.pop());
+        System.out.println("Dynamic stack :::");
+        dynamic_stack stack2=new dynamic_stack(5);
+        stack2.push(0);
+        stack2.push(1);
+        stack2.push(2);
+        stack2.push(3);
+        stack2.push(4);
+        stack2.push(5);
+        stack2.push(6);
+        System.out.println(stack2.pop());
+        System.out.println(stack2.pop());
+        System.out.println(stack2.pop());
     }
 }
